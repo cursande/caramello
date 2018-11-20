dd741b9538823a6d427b84d26b43a164630c7656
Alex F <adfarries@gmail.com>
2018-11-20 19:33:24 +1100

    Check that to is called with valid matcher

--- lib/caramello/contention.rb
+++ lib/caramello/contention.rb
@@ -3,17 +3,33 @@ require 'pry'
 module Caramello
   class Contention
 
-    # Raised when an it block does not not meet the set condition.
+    # Raised when an it block does not not meet the set condition
     class ConditionNotMetError < StandardError; end
+
+    # To avoid weird errors further on, we prevent invalid matchers
+    # being passed in
+    class InvalidMatcherError < StandardError; end
 
     def initialize(value)
       @value = value
     end
 
     def to(matcher)
+      valid_matcher?(matcher)
       return if matcher.condition_met?(@value)
 
       raise ConditionNotMetError, matcher.fail_message
+    end
+
+    private
+
+    def valid_matcher?(matcher)
+      # TODO: Make this nicer
+      Caramello::Matcher.constants.each do |klass|
+        return if matcher.instance_of?(Caramello::Matcher.const_get(klass))
+      end
+
+      raise InvalidMatcherError, 'Matcher not recognised'
     end
   end
 end
--- spec/lib/caramello/contention_spec.rb
+++ spec/lib/caramello/contention_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Caramello::Contention do
   subject { described_class }
 
   describe '#to' do
-    context 'when the contention is passed a matcher object' do
+    context 'when the contention is passed a matcher instance' do
       let(:matcher) { Caramello::Matcher::MatchEquality }
 
       it 'does not raise if the condition is met' do
@@ -14,6 +14,14 @@ RSpec.describe Caramello::Contention do
 
       it 'raises if the condition is not met' do
         expect{ subject.new('foo').to matcher.new('bar') }.to raise_error(subject::ConditionNotMetError)
+      end
+    end
+
+    context 'when the contention is passed any other data type' do
+      let(:matcher) { 'some string' }
+
+      it 'raises an InvalidMatcherError' do
+        expect { subject.new('foo').to matcher }.to raise_error(subject::InvalidMatcherError)
       end
     end
   end
