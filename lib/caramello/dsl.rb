ad8444517d4f015bd9c23d362e836899bd56f6b4
Alex F <adfarries@gmail.com>
2018-11-24 22:40:31 +1100

    Add contend to dsl

--- lib/caramello/dsl.rb
+++ lib/caramello/dsl.rb
@@ -1,12 +1,11 @@
 module Caramello
   module DSL
-    class TestBehaviour
-      def initialize(string, &block)
-      end
+    def contend(value)
+      Caramello::Contention.new(value)
+    end
 
-      def equal(value)
-        Caramello::Matcher::MatchEquality.new(value)
-      end
+    def equal(value)
+      Caramello::Matcher::MatchEquality.new(value)
     end
   end
 end
